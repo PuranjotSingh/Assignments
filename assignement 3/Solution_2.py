48f0dcc558d11b98ea95ac2fecc31029bcc66380
PuranjotSingh <120919736+PuranjotSingh@users.noreply.github.com>
2022-12-22 15:50:49 +0530

    Update Solution_2.py

--- assignement 3/Solution_2.py
+++ assignement 3/Solution_2.py
@@ -1,81 +1,37 @@
-print("Enter Date below to get date of next day.\n")
-
-def leapyear(x):
-    # leap year condition
-    if (x%400)==0 or ((x%100!=0) and (x%4==0)):
-        return True
+a=int(input("Enter Date[1-31]:"))
+b=int(input("Enter Month[1-12]:"))
+c=int(input("Enter Year[1800-2025]"))
+if a>31:
+    print("Error input correct date")
+    exit()
+#seeing if input year is a leap year or not
+if (c%4==0):
+    leapyear=True
+else:
+    leapyear=False
+#defining month lengths
+if b in [1,3,5,7,8,10,12]:
+    monthlength=31
+elif b in [4,6,9,11]:
+    monthlength=30
+if b==2:
+    if leapyear:
+        monthlength=29
     else:
-        return False
+        monthlength=28
 
-day=int(input("Enter Day [1-31]:"))
-month=int(input("Enter Month [1-12]:"))
-year=int(input("Enter Year [1800-2025]:"))
-#condition 1
-if day<1 or day>31 or month<1 or month>12 or year<1800 or year>2025:
-    condition1=False
+if a>monthlength:
+    print("Error please input a correct date")
+    exit()
+#adding a day
+if a<monthlength:
+    a=a+1
 else:
-    condition1=True
+    a=1
+    if b==12:
+        b=1
+        c=c+1
+    else:
+        b=b+1
 
-
-month_31 = [1, 3, 5, 7, 9, 12, 8]  #List containing month with 31 days
-month_30 = [4, 6, 10, 11]    #List containing month with 30 days
-#for day entered 31 does not present in that month
-c1a= day==31 and (month in month_30)
-#for day entered greater than 29 in month february i.e 2
-c1b= day>29  and month==2
-#for day entered greater than 28 in february in non leapyear
-c1c= day==29 and (not leapyear(year)) and month==2
-if c1a or c1b or c1c :
-    condition2=False
-else:
-    condition2=True
-
-if c1a:
-    print(f"\nError\n{day} day can't be in month {month}")
-elif c1b:
-    print(f"\nError\n{day} day can't be in month {month}")
-elif c1c:
-    print(f"\nError\n{day} day can't be in month {month} as the year {year} in not leapyear")
-elif condition1==False:
-    print(f"\nError\nPlease enter date in range day[1-31], month[1-12], year[1800-2025] ")
-
-
-if condition1==True and condition2==True :
-    month_31 = [1, 3, 5, 7,8, 9, 12]  #List containing month with 31 days
-    month_30 = [4, 6,10, 11]    #List containing month with 30 days
-    #For month with 31 days
-    if (month in month_31) == True:
-        if day == 31:
-            day = 1
-            month = month + 1
-        elif 1 <= day <= 30:
-            day = day + 1
-    
-    elif (month in month_30) == True:
-        if day == 30 and month == 12:
-            day = 1
-            month = 1
-            year = year + 1
-        elif day == 30 and month != 12:
-            day = 1
-            month = month + 1
-        elif 1 <= day <= 29:
-            day = day + 1
-    
-    elif month == 2:
-       
-        if leapyear(year) == True:
-            if day == 29:
-                day = 1
-                month = month + 1
-            elif 1 <= day <= 28:
-                day = day + 1
-       
-        elif leapyear(year) == False:
-            if day == 28:
-                day = 1
-                month = month + 1
-            elif 1 <= day <= 27:
-                day = day + 1
-    
-    print(f"\nNext Date in format Day/Month/Year is: {day}/{month}/{year}")
+print("The Next date will be",a,"/",b,"/",c)
